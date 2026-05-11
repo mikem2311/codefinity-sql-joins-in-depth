3ca888942f80b847060f8155e975876573d1c071
mikem2311 <>
2026-05-11 01:31:12 +0000

    Challenge: Unmatched Records Report

--- core_join_types_and_their_applications/challenge_unmatched_records_report/query.sql
+++ core_join_types_and_their_applications/challenge_unmatched_records_report/query.sql
@@ -1,5 +1,22 @@
 -- Write your code here
-SELECT 
-
-FROM customers c FULL OUTER JOIN orders o
+SELECT c.customer_id, 
+    c.name as customer_name, 
+    o.order_id, 
+    o.order_date,
+CASE
+    WHEN o.order_id IS NULL THEN 'Customer without orders'
+    WHEN c.customer_id IS NULL THEN 'Order without customer'
+    ELSE 'Matched'
+    END AS match_status
+FROM customers c 
+FULL OUTER JOIN orders o
 ON c.customer_id = o.customer_id
+
+ORDER BY
+    CASE
+        WHEN c.customer_id IS NULL 
+             OR o.order_id IS NULL THEN 0
+        ELSE 1
+    END,
+    c.customer_id,
+    o.order_id;
